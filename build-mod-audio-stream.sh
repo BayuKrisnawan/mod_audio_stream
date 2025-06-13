3066bb08ef10db86d0d9e1660720fc3ae2e207c2
BayuKrisnawan <krisna@gmail.com>
2025-06-13 15:13:50 +0700

    Build From Sources

--- build-mod-audio-stream.sh
+++ build-mod-audio-stream.sh
@@ -6,17 +6,22 @@
 #    && cd mod_audio_stream \
 #    && sudo bash ./build-mod-audio-stream.sh
 
-apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev
+# apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev
+apt-get -y install libssl-dev zlib1g-dev libspeexdsp-dev
 
 git submodule init
 git submodule update
+PKG_CONFIG_PATH=/usr/lib/pkgconfig
 
-FS_PKGCONFIG=/usr/local/freeswitch/lib/pkgconfig
+export PKG_CONFIG_PATH
+FS_PKGCONFIG=/usr/lib/pkgconfig
 if [ -d "$FS_PKGCONFIG" ]; then
     export PKG_CONFIG_PATH=$FS_PKGCONFIG
 fi
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+CMAKE_INSTALL_PREFIX=/usr 
+# --install-prefix=/usr
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release  ..
 make
 make install
